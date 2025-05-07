64d5872ffad351f58e83150173dbcd0ce6dbcdc2
irishdiane <idliniego@gmail.com>
2025-05-07 23:37:50 +0800

    Refactor 404.html and index.js: streamline redirection logic and enhance path handling

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,12 @@ import './index.css';
 import AppRouter from './route';
 import reportWebVitals from './reportWebVitals';
 
+const urlParams = new URLSearchParams(window.location.search);
+const redirectedPath = urlParams.get("redirect");
+if (redirectedPath) {
+  window.history.replaceState({}, "", redirectedPath);
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
