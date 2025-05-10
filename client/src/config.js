3094763afedb41091ac18950a66768bb0b344805
irishdiane <idliniego@gmail.com>
2025-05-11 00:00:21 +0800

    Create config.js

---
+++ client/src/config.js
@@ -0,0 +1,7 @@
+// config.js
+const API_URL = 'https://170d-23-97-62-138.ngrok-free.app';
+
+export default API_URL;
+// You can also export multiple configurations if needed
+// export const API_URL = 'https://170d-23-97-62-138.ngrok-free.app';
+// export const OTHER_CONFIG = '...';
