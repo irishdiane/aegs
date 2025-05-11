c8dda3fe507428e782b05a433191a012bc524b74
irishdiane <idliniego@gmail.com>
2025-05-11 14:06:14 +0800

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 
-const API_URL = 'https://9555-23-97-62-112.ngrok-free.app';
+const API_URL = 'https://dd2d-23-97-62-112.ngrok-free.app';
 
 console.log('API URL being used:', API_URL); // Helpful for debugging
 
