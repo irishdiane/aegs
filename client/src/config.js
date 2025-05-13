98a8f997a0c99846742ae11423333a1005696872
rubric-aegs <idliniego@gmail.com>
2025-05-13 10:35:00 +0800

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 
-const API_URL = 'https://8122-23-97-62-116.ngrok-free.app';
+const API_URL = 'https://6d4e-23-97-62-148.ngrok-free.app';
 
 console.log('API URL being used:', API_URL); // Helpful for debugging
 
